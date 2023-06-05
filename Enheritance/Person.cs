1704e83bb205f342b8111322e84e211857716e05
RyoZAgta <ryoagta01@gmail.com>
2023-06-05 18:38:32 +0700

    Enheritance

---
+++ Enheritance/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enheritance
+{
+    public class Person
+    {
+        //Properti
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //fungsi
+        public Person()
+        {
+
+        }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+
+            Console.WriteLine(" Nama saya {0}, dan umur saya {1}, tahun\n", Name, Age);
+            Console.WriteLine();
+        }
+        public void InfoPerson()
+        {
+            Console.WriteLine(" Nama saya {0}, dan umur saya {1}, tahun\n", Name, Age);
+            Console.WriteLine();
+        }
+    }
+    //Class turunan
+    public class Karyawan : Person
+    {
+        //Properti
+        public int KaryawanID { get; set; }
+        public string Subject { get; set; }
+
+        //Overide Method 
+        public Karyawan()
+        {
+
+        }
+
+        public Karyawan(string Subject)
+        {
+            this.Subject = Subject;
+            Console.WriteLine("Saya disini sebagai {0}", this.Subject);
+            Console.WriteLine();
+        }
+        public void InfoKaryawan()
+        {
+            Console.WriteLine("ID Karyawan : {0} \nNama Karyawan : {1}", KaryawanID, Subject);
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Enheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using Enheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.Name = "Ryo Agata ";
+        person1.Age = 20;
+        person1.InfoPerson();
+
+        //Inheritance
+        Karyawan karyawan1 = new Karyawan();
+        karyawan1.KaryawanID = 22114831;
+        karyawan1.Subject = "Ryo Agata Prasetyo";
+        karyawan1.InfoKaryawan();
+
+        Karyawan karyawan2 = new Karyawan("Direktur Utama");
+    }
+}
